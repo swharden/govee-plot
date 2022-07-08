803c701ae3ba9c3a37c302de4a949d9d0a98f3d5
Scott W Harden <swharden@gmail.com>
2022-07-08 17:51:17 -0400

    Govee.Core

---
+++ src/Govee.Core/DataFile.cs
@@ -0,0 +1,29 @@
+﻿namespace Govee.Core;
+
+public static class DataFile
+{
+    public static Reading[] GetReadings(string filePath)
+    {
+        string[] lines = File.ReadAllLines(filePath);
+
+        List<Reading> readings = new();
+
+        foreach (string line in lines)
+        {
+            if (line.StartsWith("T") || line.Length < 20)
+                continue;
+
+            string[] parts = line.Split(",");
+            Reading reading = new()
+            {
+                Timestamp = DateTime.Parse(parts[0]),
+                Tempearture = double.Parse(parts[1]),
+                Humidity = double.Parse(parts[2]),
+            };
+
+            readings.Add(reading);
+        }
+
+        return readings.ToArray();
+    }
+}
---
+++ src/Govee.Core/Reading.cs
@@ -0,0 +1,8 @@
+﻿namespace Govee.Core;
+
+public struct Reading
+{
+    public DateTime Timestamp { get; init; }
+    public double Tempearture { get; init; }
+    public double Humidity { get; init; }
+}
